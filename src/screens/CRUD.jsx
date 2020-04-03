eb3db3a92ddc47a9350d1d3a0489b2306f1c1fb3
Abdul Samad <abu18sam@gmail.com>
2020-04-04 04:08:46 +0530

    EDIT COMPLETE

--- src/screens/CRUD.jsx
+++ src/screens/CRUD.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
-let baseUrl="https://reqres.in/api/";
+import { Modal, ModalBody, Form, ModalHeader, ModalFooter, FormGroup, Input, Col, Row, Label } from 'reactstrap';
+
+let baseUrl = "https://reqres.in/api/";
 
 export default class CRUD extends Component {
 
@@ -8,6 +10,8 @@ export default class CRUD extends Component {
         super(props);
         this.state = {
             usersList: [],
+            viewType: "add",
+            isOpenModal: false,
         }
     }
 
@@ -16,7 +20,7 @@ export default class CRUD extends Component {
     }
 
     async getUsersList() {
-        let data = await fetch(baseUrl+"users?page=1");
+        let data = await fetch(baseUrl + "users?page=1");
         let users = await data.json();
         this.setState({ usersList: users.data });
     }
@@ -30,23 +34,29 @@ export default class CRUD extends Component {
     }
 
     handleDelete = async (user) => {
-        let url=`${baseUrl}users/${user.id}`;
-        let response= await fetch(url, {
-            method:"DELETE",
-            headers:{
-                "Content-type":"application/json"
+        let url = `${baseUrl}users/${user.id}`;
+        let response = await fetch(url, {
+            method: "DELETE",
+            headers: {
+                "Content-type": "application/json"
             }
         });
-        let data=await response.json();
-        if(data){
+        let data = await response.json();
+        if (data) {
             alert("User Deleted Successfully")
         }
-        console.log('response=> ',data);
+        console.log('response=> ', data);
+    }
+
+    toggleModal = () => {
+        this.setState(prevState => ({ isOpenModal: !prevState.isOpenModal }));
     }
 
     render() {
 
-        let { usersList } = this.state;
+        let { usersList, viewType, isOpenModal } = this.state;
+
+        console.log("states==> ", isOpenModal);
 
         const TableRow = (props) => {
             return (
@@ -68,32 +78,60 @@ export default class CRUD extends Component {
         }
 
         return (
-            <div className="container">
-                <div className="row text-right p-3">
-                    <button type="button" class="btn btn-success">Add User</button>
-                </div>
-                <div className="row">
+            <div>
 
-                    <table className="table-responsive user-table">
-                        <thead>
-                            <tr>
-                                <td>ID</td>
-                                <td>Email</td>
-                                <td>First Name</td>
-                                <td>Last Name</td>
-                                <td>Avatar</td>
-                                <td>Actions</td>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                usersList.map((user, index) => {
-                                    return (<TableRow user={user} />);
-                                })
-                            }
-                        </tbody>
-                    </table>
+
+                <div className="container">
+                    <div className="row text-right p-3">
+                        <button type="button" onClick={this.toggleModal} class="btn btn-success">Add User</button>
+                    </div>
+                    <div className="row">
+
+                        <table className="table-responsive user-table">
+                            <thead>
+                                <tr>
+                                    <td>ID</td>
+                                    <td>Email</td>
+                                    <td>First Name</td>
+                                    <td>Last Name</td>
+                                    <td>Avatar</td>
+                                    <td>Actions</td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    usersList.map((user, index) => {
+                                        return (<TableRow user={user} />);
+                                    })
+                                }
+                            </tbody>
+                        </table>
+                    </div>
+
+
                 </div>
+                {/* Add Modal */}
+                <Modal isOpen={isOpenModal} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>
+                        {viewType == "add" ? "Add User" : "Edit User"}
+                    </ModalHeader>
+
+                    <ModalBody>
+                        <Form>
+                            <Row>
+                                <FormGroup>
+                                    <Col><Label for="email">Email</Label></Col>
+                                    <Col><Input type="email" name="email" id="email" placeholder="email" /></Col>
+                                </FormGroup>
+                            </Row>
+                        </Form>
+
+                    </ModalBody>
+
+                    <ModalFooter>
+
+                    </ModalFooter>
+                </Modal>
             </div>
         )
     }
